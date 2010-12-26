db225775c5ca296e198bb0aca8066981d7dc5257
ornicar <thibault.duplessis@gmail.com>
2010-12-26 20:40:23 +0100

    Add controller, routing and template

--- Controller/MessageController.php
+++ Controller/MessageController.php
@@ -6,4 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MessageController extends Controller
 {
+    public function listAction()
+    {
+        $user = $this->get('security.context')->getUser();
+        $messages = $this->get('ornicar_message.repository.message')->findRecentByUser($user);
+
+        return $this->render('MessageBundle:Message:list.twig', array(
+            'messages' => $messages
+        ));
+    }
 }
