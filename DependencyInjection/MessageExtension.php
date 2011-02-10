bfafc0eaf015bb5da643d277249807455f5f24f3
ornicar <thibault.duplessis@gmail.com>
2011-02-09 23:54:48 -0800

    Update dependency injection extension to make use of FileLocator

--- DependencyInjection/MessageExtension.php
+++ DependencyInjection/MessageExtension.php
@@ -4,33 +4,33 @@ namespace Bundle\Ornicar\MessageBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\FileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\FileLocator;
 
 class MessageExtension extends Extension
 {
     public function configLoad(array $configs, ContainerBuilder $container)
     {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        foreach (array('model', 'controller', 'form', 'twig', 'messenger', 'paginator') as $basename) {
+            $loader->load(sprintf('%s.xml', $basename));
+        }
+
         foreach ($configs as $config) {
-            $this->doConfigLoad($config, $container);
+            $this->doConfigLoad($config, $container, $loader);
         }
     }
 
-    public function doConfigLoad(array $config, ContainerBuilder $container)
+    public function doConfigLoad(array $config, ContainerBuilder $container, FileLoader $loader)
     {
-        if(!$container->hasDefinition('ornicar_message.repository.message')) {
-            // ensure the db_driver is configured
-            if (!isset($config['db_driver'])) {
-                throw new \InvalidArgumentException('The db_driver parameter must be defined');
-            } elseif (!in_array($config['db_driver'], array('orm', 'odm'))) {
-                throw new \InvalidArgumentException(sprintf('The db_driver "%s" is not supported (choose either "odm" or "orm")', $config['db_driver']));
-            }
-            $loader = new XmlFileLoader($container, __DIR__.'/../Resources/config');
-            // load all service configuration files (the db_driver first)
-            foreach (array($config['db_driver'], 'model', 'controller', 'form', 'twig', 'messenger', 'paginator') as $basename) {
-                $loader->load(sprintf('%s.xml', $basename));
-            }
+        // ensure the db_driver is configured
+        if (!isset($config['db_driver'])) {
+            throw new \InvalidArgumentException('The db_driver parameter must be defined');
+        } elseif (!in_array($config['db_driver'], array('orm', 'odm'))) {
+            throw new \InvalidArgumentException(sprintf('The db_driver "%s" is not supported (choose either "odm" or "orm")', $config['db_driver']));
         }
+        $loader->load(sprintf('%s.xml', $config['db_driver']));
 
         // ensure the user model class is configured
         if (!isset($config['class']['model']['message'])) {
