58f08886bad6d4eca48e0856d3faf6015b269177
Alex Kovalevych <alexkovalevych@gmail.com>
2011-07-23 02:31:28 +0300

    added reply message authorization

---
+++ Validator/ReplyAuthorization.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ornicar\MessageBundle\Validator;
+
+use Symfony\Component\Validator\Constraint;
+
+class ReplyAuthorization extends Constraint
+{
+    public $message = 'You are not allowed to reply to this this message';
+
+    public function validatedBy()
+    {
+        return 'ornicar_message.validator.reply_authorization';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
---
+++ Validator/ReplyAuthorizationValidator.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Ornicar\MessageBundle\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Ornicar\MessageBundle\Security\AuthorizerInterface;
+use Ornicar\MessageBundle\Security\ParticipantProviderInterface;
+
+class ReplyAuthorizationValidator extends ConstraintValidator
+{
+    /**
+     * @var AuthorizerInterface
+     */
+    protected $authorizer;
+
+    /**
+     * Constructor
+     *
+     * @param AuthorizerInterface $authorizer
+     * @param ParticipantProviderInterface $participantProvider
+     */
+    public function __construct(AuthorizerInterface $authorizer, ParticipantProviderInterface $participantProvider)
+    {
+        $this->authorizer = $authorizer;
+        $this->participantProvider = $participantProvider;
+    }
+
+    /**
+     * Indicates whether the constraint is valid
+     *
+     * @param object     $value
+     * @param Constraint $constraint
+     */
+    public function isValid($value, Constraint $constraint)
+    {
+        $sender = $this->participantProvider->getAuthenticatedParticipant();
+        $recipient = $value->getThread()->getOtherParticipant($sender);
+        if (!$recipient) {
+            return true;
+        }
+        if (!$this->authorizer->canMessageParticipant($recipient)) {
+            $this->setMessage($constraint->message);
+            return false;
+        }
+
+        return true;
+    }
+}
+
