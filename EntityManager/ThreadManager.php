0581de67e0aa094d9e0efb76431824c2d1e9f8d1
Nicolas Badey <speedbad@free.fr>
2011-12-02 15:51:50 +0100

    Else we have "1970 ..." in lastMessageDate instead of NULL and new topic without response are diplayed in sender's inbox list with wrong sort (because we have "where lastmessage IS NOT NULL" in getParticipantSentThreadsQueryBuilder)
    If we want new topic whitout response display in sender's inbox list, just add in more this fix, a constructor in ThreadMetadata with
    $this->lastMessageDate = new \DateTime();

--- EntityManager/ThreadManager.php
+++ EntityManager/ThreadManager.php
@@ -363,10 +363,11 @@ class ThreadManager extends BaseThreadManager
                     $timestamp = max($timestamp, $message->getTimestamp());
                 }
             }
-
-            $date = new \DateTime();
-            $date->setTimestamp($timestamp);
-            $meta->setLastMessageDate($date);
+            if ($timestamp) {
+                $date = new \DateTime();
+                $date->setTimestamp($timestamp);
+                $meta->setLastMessageDate($date);
+            }
         }
     }
 
