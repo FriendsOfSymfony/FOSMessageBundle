b53872f6980fa80a0d9f19104e6813d3b6f63cad
Jeremy Mikola <jmikola@gmail.com>
2012-01-17 17:07:55 -0500

    [Sender] Ensure threads are undeleted before saving
    As the code was, thread undeletion depended on a POST_SEND listener flushing the object manager.

--- Sender/Sender.php
+++ Sender/Sender.php
@@ -45,15 +45,23 @@ class Sender implements SenderInterface
     }
 
     /**
-     * Sends the message by persisting it to the message manager and undeletes the thread for all participants
+     * Sends the message by persisting it to the message manager and undeletes
+     * the thread for all participants.
      *
      * @param MessageInterface $message
      */
     public function send(MessageInterface $message)
     {
         $this->threadManager->saveThread($message->getThread(), false);
-        $this->messageManager->saveMessage($message);
+        $this->messageManager->saveMessage($message, false);
+
+        /* Note: Thread::setIsDeleted() depends on metadata existing for all
+         * thread and message participants, so both objects must be saved first.
+         * We can avoid flushing the object manager, since we must save once
+         * again after undeleting the thread.
+         */
         $message->getThread()->setIsDeleted(false);
+        $this->messageManager->saveMessage($message);
 
         $this->dispatcher->dispatch(OrnicarMessageEvents::POST_SEND, new MessageEvent($message));
     }
