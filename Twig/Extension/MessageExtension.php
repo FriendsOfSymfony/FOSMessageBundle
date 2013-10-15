bfb64e98c3ccc24e3f8e97a869903faa5d924acd
michiel <michiel@Michiel>
2013-10-15 21:13:47 +0200

    undelete actions in routing and controller
    updates on twig
    
    modified threads_list
    
     try to fix the formatting changes
    
    try to fix the formatting changes
    
    fixed an extra space
    
    removed delete method
    
    readded docs
    
    hmm bad at english
    
    cs
    
    cs
    
    cs
    
    lol
    
    added new  line to end of file
    
    updated tabs in threads list
    
    updates
    
    added function
    
    small updates

--- Twig/Extension/MessageExtension.php
+++ Twig/Extension/MessageExtension.php
@@ -5,18 +5,22 @@ namespace FOS\MessageBundle\Twig\Extension;
 use FOS\MessageBundle\Security\ParticipantProviderInterface;
 use FOS\MessageBundle\Model\ReadableInterface;
 use FOS\MessageBundle\Provider\ProviderInterface;
+use FOS\MessageBundle\Model\ThreadInterface;
+use FOS\MessageBundle\Security\AuthorizerInterface;
 
 class MessageExtension extends \Twig_Extension
 {
     protected $participantProvider;
     protected $provider;
+    protected $authorizer;
 
     protected $nbUnreadMessagesCache;
 
-    public function __construct(ParticipantProviderInterface $participantProvider, ProviderInterface $provider)
+    public function __construct(ParticipantProviderInterface $participantProvider, ProviderInterface $provider, AuthorizerInterface $authorizer)
     {
         $this->participantProvider = $participantProvider;
         $this->provider = $provider;
+        $this->authorizer = $authorizer;
     }
 
     /**
@@ -28,7 +32,9 @@ class MessageExtension extends \Twig_Extension
     {
         return array(
             'fos_message_is_read'  => new \Twig_Function_Method($this, 'isRead'),
-            'fos_message_nb_unread' => new \Twig_Function_Method($this, 'getNbUnread')
+            'fos_message_nb_unread' => new \Twig_Function_Method($this, 'getNbUnread'),
+            'fos_message_can_delete_thread' => new \Twig_Function_Method($this, 'canDeleteThread'),
+            'fos_message_deleted_by_participant' => new \Twig_Function_Method($this, 'isThreadDeletedByParticipant')
         );
     }
 
@@ -40,6 +46,31 @@ class MessageExtension extends \Twig_Extension
     public function isRead(ReadableInterface $readable)
     {
         return $readable->isReadByParticipant($this->getAuthenticatedParticipant());
+    }
+    
+    
+    /**
+     * Checks if the participant can mark a thread as deleted
+     * 
+     * @param ThreadInterface $thread
+     * 
+     * @return boolean true if participant can mark a thread as deleted, false otherwise
+     */
+    public function canDeleteThread(ThreadInterface $thread)
+    {
+        return $this->authorizer->canDeleteThread($thread);
+    }
+    
+    /**
+     * Checks if the participant has marked the thread as deleted
+     * 
+     * @param ThreadInterface $thread
+     * 
+     * @return boolean true if participant has marked the thread as deleted, false otherwise
+     */
+    public function isThreadDeletedByParticipant(ThreadInterface $thread)
+    {
+       return $thread->isDeletedByParticipant($this->getAuthenticatedParticipant());
     }
 
     /**
