ad417c2ee83021f0dcea229c5a533d28ebcba84d
Tim Nagel <tim@nagel.com.au>
2013-10-26 12:23:47 +1100

    Fix tests

--- Tests/Twig/MessageExtensionTest.php
+++ Tests/Twig/MessageExtensionTest.php
@@ -2,6 +2,7 @@
 
 namespace FOS\MessageBundle\Tests\Twig;
 
+use FOS\MessageBundle\Tests\Model\Thread;
 use FOS\MessageBundle\Twig\MessageExtension;
 use Mockery as m;
 
@@ -11,6 +12,11 @@ class MessageExtensionTest extends \PHPUnit_Framework_TestCase
      * @var MessageExtension
      */
     private $extension;
+
+    /**
+     * @var \FOS\MessageBundle\Security\AuthorizerInterface|\Mockery\MockInterface
+     */
+    private $authorizer;
 
     /**
      * @var \FOS\MessageBundle\Security\ParticipantProviderInterface|\Mockery\MockInterface
@@ -44,14 +50,43 @@ class MessageExtensionTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(5, $this->extension->getNbUnread());
         $this->assertEquals(5, $this->extension->getNbUnread());
+        $this->assertArrayHasKey('fos_message_nb_unread', $this->extension->getFunctions());
+    }
+
+    public function testCanDeleteThread()
+    {
+        $thread = new Thread;
+
+        $this->authorizer->shouldReceive('canDeleteThread')
+            ->with($thread)
+            ->andReturn(true)
+            ->once();
+
+        $this->assertTrue($this->extension->canDeleteThread($thread));
+        $this->assertArrayHasKey('fos_message_can_delete_thread', $this->extension->getFunctions());
+    }
+
+    public function testIsThreadDeletedByParticipant()
+    {
+        $this->participantProvider->shouldReceive('getAuthenticatedParticipant')
+            ->andReturn($participant = $this->getParticipant());
+
+        $thread = m::mock('FOS\\MessageBundle\\Model\\ThreadInterface');
+        $thread->shouldReceive('isDeletedByParticipant')
+            ->with($participant)
+            ->andReturn(false);
+
+        $this->assertFalse($this->extension->isThreadDeletedByParticipant($thread));
+        $this->assertArrayHasKey('fos_message_deleted_by_participant', $this->extension->getFunctions());
     }
 
     protected function setUp()
     {
+        $this->authorizer = m::mock('FOS\\MessageBundle\\Security\\AuthorizerInterface');
         $this->participantProvider = m::mock('FOS\\MessageBundle\\Security\\ParticipantProviderInterface');
         $this->provider = m::mock('FOS\\MessageBundle\\Provider\\ProviderInterface');
 
-        $this->extension = new MessageExtension($this->participantProvider, $this->provider);
+        $this->extension = new MessageExtension($this->participantProvider, $this->provider, $this->authorizer);
     }
 
     /**
